034d332a5d412f179f49a6476d1a0ca80b4e5107
josea <josea@192.168.1.128>
2026-03-25 00:06:10 +0100

    Commit division por cero

--- Calculadora/src/calculadora.java
+++ Calculadora/src/calculadora.java
@@ -1,8 +1,11 @@
 
 public class calculadora {
 	
-	public int sumar(int a, int b) { 
-		return a + b;	    
-	}
+	public double dividir(int a, int b) {
+	    if (b == 0) {
+	      throw new IllegalArgumentException("No se puede dividir entre cero");
+	    }
+	    return (double) a / b;
+	  }
 		
 }
